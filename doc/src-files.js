3d899c4dfd4e4f7836438452bb5157e0cfbec8a5
Daniel Pezely <daniel@mxlmidi.org>
2026-01-02 15:38:25 -0700

    Add doc, moved from ./target/doc/

---
+++ doc/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["mnx_canary",["",[],["lib.rs","mnx.rs"]]]]');
+//{"start":19,"fragment_lengths":[42]}
---
+++ doc/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mnx_canary",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"mnx_canary/mnx/error/struct.ConversionError.html\" title=\"struct mnx_canary::mnx::error::ConversionError\">ConversionError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[318]}
